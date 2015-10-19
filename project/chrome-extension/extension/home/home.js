0f101f83ed8515ceffb9269950ddc74ee2cfb41b
Igor Dudschenko <igor.dudschenko@rwth-aachen.de>
2015-10-19 16:22:12 +0200

    fixes and comm

--- project/chrome-extension/extension/home/home.js
+++ project/chrome-extension/extension/home/home.js
@@ -1,28 +1,42 @@
-require.config({
-    baseUrl: '../',
-    paths: {
-        "angular":'vendors/angular/angular',
-        "jquery": 'vendors/jquery/dist/jquery',
-        "moment": 'vendors/moment/min/moment-with-locales',
-        "noty":   'vendors/noty/js/noty/packaged/jquery.noty.packaged'
-    },
-    shim: {
-        'angular': {
-            exports: 'angular'
-        },
-        jquery:{
-            exports: 'jquery'
-        },
-        noty:{
-            deps: ['jquery'],
-            exports: 'noty'
-        }
-    }
-});
-require(['apps/home','jquery'], function(home, $){
-    home.start();
-    chrome.extension.getBackgroundPage().getCurrentUrl(function(url){
-        console.log('url:',url);
-        $('#data').val(url);
-    });
+document.addEventListener('DOMContentLoaded', function() {
+var homeButtonId = document.getElementById('homeButtonId');
+homeButtonId.addEventListener('click', function(){
+	var version = "4";
+	var username = localStorage["userName"];
+	var password = localStorage["userPassword"];
+	var appId = "58";
+	var appSecret = "5vzgj870p5rizch61j89vro6if8yexl6pj11ckphzi4pkb86x5";
+	var callbackURL = "";
+	var refreshToken = "1";
+  var lc = new LearningContext("http://api.learning-context.de",
+    version,
+    appId, 
+    appSecret,
+	callbackURL,
+	refreshToken
+    );
+		
+	var topic = document.getElementById("topic");
+	var data = document.getElementById("data");
+	var rate = rating.children[rating.selectedIndex].value;
+	
+	alert("Username: " + username + " Password: " + password + " Topic: " + topic + " Data: " + data + " Rating: " + rate);
+	
+	//TODO: GET URL AND ANALYZE CONTENT
+	//ALSO: Give Topic Suggestion
+	
+	var result = lc.get("events", '{"model":"COMPLETE"}');
+	console.log(result);
+	
+	var event = new Event("START", "2015-10-15 12:12:12", "WEBBASED", "PRIVATE", "LECTURE");
+	var entity1 = new Entity(topic);
+	var entity2 = new Entity(data);
+	var entity3 = new Entity(rate);
+	event.addEntity(entity1);
+	event.addEntity(entity2);
+	event.addEntity(entity3);
+	
+	var json = "[" + event.toJson() + "]";
+	console.log(lc.post("events", json));
+	});	
 });
--- project/chrome-extension/extension/options/login.js
+++ project/chrome-extension/extension/options/login.js
@@ -1,28 +1,39 @@
-angular.module('controllerAsExample', [])
-  .controller('myController', myController);
-
-function myController() {
-  this.user.name = "";
-  this.user.password = "";
-}
-
-myController.prototype.save = function() {
+document.addEventListener('DOMContentLoaded', function() {
+var buttonId = document.getElementById('buttonId');
+buttonId.addEventListener('click', function(){
+	var userName = document.getElementById("name");
+	var userPassword = document.getElementById("password");
+	
+	localStorage["userName"] = userName.value;
+	localStorage["userPassword"] = userPassword.value;
+	
+	var version = "4";
+	var username = "IgorDud";
+	var password = "test";
+	var appId = "58";
+	var appSecret = "5vzgj870p5rizch61j89vro6if8yexl6pj11ckphzi4pkb86x5";
+	var callbackURL = "";
+	var refreshToken = "1";
   var lc = new LearningContext("http://api.learning-context.de",
-    version, 
+    version,
     appId, 
     appSecret,
-    callbackURL,
-    refreshToken);
+	callbackURL,
+	refreshToken
+    );
 	
 	var result = lc.get("events", '{"model":"COMPLETE"}');
 	console.log(result);
 	
-	var event = new Event("START", "2015-10-15 12:12:12", "MOBILE", "PRIVATE", "POSITION");
+	/*var event = new Event("START", "2015-10-15 12:12:12", "MOBILE", "PRIVATE", "POSITION");
 	var entity1 = new Entity("lng", "6.44");
 	var entity2 = new Entity("lat", "50.33");
 	event.addEntity(entity1);
 	event.addEntity(entity2);
 	
 	var json = "[" + event.toJson() + "]";
-	console.log(lc.post("events", json));
-};
+	console.log(lc.post("events", json));*/ // Send something?
+	});	
+});
+
+
